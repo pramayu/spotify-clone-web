ca8cb8fc411a7f6da85f708c5f45b05da9975190
pramayu <gedepramayu@gmail.com>
2017-02-12 19:34:51 +0800

    login page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.render('./users/login', {layout: 'auth', title: "Login ~ Spotify"});
+router.get('/', function(req, res, next) {
+  res.render('./users/main', {layout: 'auth', title: "Login ~ Spotifer."});
+});
+
+router.get('/login', function(req, res, next){
+  res.render('./users/login', {layout: 'auth', title: "Login ~ Spotifer."})
 });
 
 module.exports = router;
